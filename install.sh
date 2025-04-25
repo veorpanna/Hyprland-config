ffb390657ecd59175f815b1c605c27d1a545239b
veorpanna <150652742+veorpanna@users.noreply.github.com>
2025-04-25 15:06:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo pacman -S ttf-droid noto-fonts wayland hyprland
-sudo pacman -S base-devel git zsh
+sudo pacman -S base-devel git zsh xdg-user-dirs
 sudo pacman -S ttf-jetbrains-mono ttf-jetbrains-mono-nerd otf-font-awesome
 sudo pacman -S wofi nm-connection-editor blueman pavucontrol polkit-gnome hyprpaper mako waybar wlogout hypridle
 sudo pacman -S kitty vim gedit nautilus firefox   
